9fc033f4cb18b9427c7cbc41dcd315bb8efc7c58
Toru <torumoy@gmail.com>
2021-01-15 12:45:26 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Torumoy Ghoshal on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
